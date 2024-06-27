c5cbf29f2b447b0e386ca04078f101a5fa59bcbb
Jagrath Shetty <jagrathshettylxix@gmail.com>
2024-06-27 17:37:23 +0530

    component: added custom cursor

---
+++ app/Components/CustomCursor/CustomCursor.tsx
@@ -0,0 +1,28 @@
+"use client";
+import useMouseCursor from "@/app/hooks/useMouseCursor";
+import { motion } from "framer-motion";
+
+type Props = {};
+
+function CustomCursor({}: Props) {
+  const [mouse, smoothMouse] = useMouseCursor();
+  const { x, y } = mouse;
+  const { x: smoothX, y: smoothY } = smoothMouse;
+
+  return (
+    <>
+      <motion.div
+        style={{ left: smoothX, top: smoothY }}
+        animate="default"
+        className="pointer-events-none fixed top-0 z-50 h-14 w-14 overflow-hidden rounded-full bg-sky-600 mix-blend-difference"
+      ></motion.div>
+      <motion.div
+        style={{ left: x, top: y }}
+        animate="default"
+        className="pointer-events-none fixed top-0 z-50 h-4 w-4 overflow-hidden rounded-full bg-sky-600"
+      ></motion.div>
+    </>
+  );
+}
+
+export default CustomCursor;
