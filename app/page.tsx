bbecbe60d00ebd31798cfb9504077d8cae78b33c
Jagrath Shetty <jagrathshettylxix@gmail.com>
2024-06-27 19:31:09 +0530

    added components to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import AboutUs from "./Components/AboutUsSection/AboutUs";
+import CustomCursor from "./Components/CustomCursor/CustomCursor";
 import Hero from "./Components/HeroSection/Hero";
+import ServiceSection from "./Components/ServiceSection/ServiceSection";
 
 export default function Home() {
   return (
     <main className="">
       <Hero />
+      <AboutUs />
+      <ServiceSection />
+      <CustomCursor />
+      <div className="w-full h-[200vh] bg-white"></div>
     </main>
   );
 }
