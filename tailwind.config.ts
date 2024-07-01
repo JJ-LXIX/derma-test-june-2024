0d29697b46fbcfca2c3ad69a7247836bf36f83ce
Jagrath Shetty <jagrathshettylxix@gmail.com>
2024-07-01 19:53:03 +0530

    added tailwind animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,18 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      animation: {
+        "loop-scroll": "loop-scroll 50s linear infinite",
+        "smiley-spin": "smiley-spin 50s linear infinite",
+      },
+      keyframes: {
+        "loop-scroll": {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        "smiley-spin": {
+          to: { transform: "rotate(360deg)" },
+        },
       },
     },
   },
