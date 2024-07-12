086efe9e52b78be29be1d03ed4d385abfa7833c0
Jagrath Shetty <jagrathshettylxix@gmail.com>
2024-07-12 17:38:04 +0530

    ui: removed on smaller screens

--- app/Components/CustomCursor/CustomCursor.tsx
+++ app/Components/CustomCursor/CustomCursor.tsx
@@ -1,4 +1,5 @@
 "use client";
+import useDimension from "@/app/hooks/useDimension";
 import useMouseCursor from "@/app/hooks/useMouseCursor";
 import { motion } from "framer-motion";
 
@@ -8,19 +9,23 @@ function CustomCursor({}: Props) {
   const [mouse, smoothMouse] = useMouseCursor();
   const { x, y } = mouse;
   const { x: smoothX, y: smoothY } = smoothMouse;
-
+  const { isSmallScreen } = useDimension();
   return (
     <>
-      <motion.div
-        style={{ left: smoothX, top: smoothY }}
-        animate="default"
-        className="pointer-events-none fixed top-0 z-50 h-14 w-14 overflow-hidden rounded-full bg-pink-950 mix-blend-difference"
-      ></motion.div>
-      <motion.div
-        style={{ left: x, top: y }}
-        animate="default"
-        className="pointer-events-none fixed top-0 z-50 h-4 w-4 overflow-hidden rounded-full bg-sky-600"
-      ></motion.div>
+      {isSmallScreen ? null : (
+        <>
+          <motion.div
+            style={{ left: smoothX, top: smoothY }}
+            animate="default"
+            className="pointer-events-none fixed top-0 z-50 h-14 w-14 overflow-hidden rounded-full bg-pink-950 mix-blend-difference"
+          ></motion.div>
+          <motion.div
+            style={{ left: x, top: y }}
+            animate="default"
+            className="pointer-events-none fixed top-0 z-50 h-4 w-4 overflow-hidden rounded-full bg-sky-600"
+          ></motion.div>
+        </>
+      )}
     </>
   );
 }
