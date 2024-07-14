519bb5740e9661ec6bc6b161b80ed5cbe6706443
Jagrath Shetty <jagrathshettylxix@gmail.com>
2024-07-14 20:04:14 +0530

    ui: adjusted to mobile screens

--- app/Components/FooterSection/Footer.tsx
+++ app/Components/FooterSection/Footer.tsx
@@ -5,14 +5,15 @@ import { editorial } from "@/app/libs/fonts/localFonts";
 import FooterIcon from "../Icons/FooterIcon";
 import Instagram from "../Icons/Instagram";
 import Facebook from "../Icons/Facebook";
+import Link from "next/link";
 
 type Props = {};
 
 export default function Footer({}: Props) {
   return (
-    <div className="h-[40vh] w-full bg-black">
+    <div className="min-h-[40vh] w-full bg-black">
       {/* Top Moving */}
-      <div className="h-1/6 w-full flex animate-loop-scroll space-x-2 overflow-clip">
+      <div className="h-1/6 w-full flex animate-loop-scroll space-x-2 ">
         <ScrollingText />
       </div>
 
@@ -24,13 +25,13 @@ export default function Footer({}: Props) {
             <div className="h-3/4 w-full relative ">{/* <FooterIcon /> */}</div>
           </div>
           {/* Middle */}
-          <div className="w-4/6 h-full pt-0">
-            <div className="w-full h-3/4 flex">
-              <div className="h-full w-1/2 ">
+          <div className="w-4/6 h-full pt-5">
+            <div className="w-full h-3/4 flex flex-col lg:flex-row">
+              <div className="h-full w-1/2">
                 <div className="w-full h-full flex flex-col">
-                  <p className="text-8xl font-semibold">FIND US:</p>
+                  <p className="text-xl lg:text-8xl font-semibold">FIND US:</p>
                   <p
-                    className={`${editorial.className} text-2xl  text-zinc-300 uppercase`}
+                    className={`${editorial.className} text-lg lg:text-2xl  text-zinc-300 uppercase`}
                   >
                     {/* TODO: add mail link */}
                     heliosdermatology@info.com
@@ -38,20 +39,20 @@ export default function Footer({}: Props) {
                 </div>
               </div>
 
-              <div className="h-full w-1/2">
+              <div className="h-full w-full lg:w-1/2 pt-0">
                 <div className="w-full h-full flex justify-end space-x-20 px-5 py-5">
                   {/* TODO: add next link */}
                   <ul className="flex flex-col h-full space-y-2">
                     <li>
-                      <p className="text-xl cursor-pointer text-zinc-300 relative group">
+                      <p className="lg:text-xl cursor-pointer text-zinc-300 relative group">
                         Privacy
                         <span className="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-zinc-200 duration-300 group-hover:w-3/6"></span>
                         <span className="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-zinc-200 duration-300 group-hover:w-3/6"></span>{" "}
                         Policy
                       </p>
                     </li>
-                    <li className="text-xl cursor-pointer text-zinc-300">
-                      <p className="text-xl cursor-pointer text-zinc-300 relative group">
+                    <li className="lg:text-xl cursor-pointer text-zinc-300">
+                      <p className="cursor-pointer text-zinc-300 relative group">
                         Insurance
                         <span className="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-zinc-200 duration-300 group-hover:w-3/6"></span>
                         <span className="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-zinc-200 duration-300 group-hover:w-3/6"></span>
@@ -59,30 +60,30 @@ export default function Footer({}: Props) {
                     </li>
                   </ul>
                   <ul className="flex flex-col h-full space-y-2">
-                    <li className="text-xl cursor-pointer text-zinc-300">
-                      <p className="text-xl cursor-pointer text-zinc-300 relative group">
+                    <li className="lg:text-xl cursor-pointer text-zinc-300">
+                      <p className="cursor-pointer text-zinc-300 relative group">
                         Our
                         <span className="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-zinc-200 duration-300 group-hover:w-3/6"></span>
                         <span className="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-zinc-200 duration-300 group-hover:w-3/6"></span>{" "}
                         Work
                       </p>
                     </li>
-                    <li className="text-xl cursor-pointer text-zinc-300">
-                      <p className="text-xl cursor-pointer text-zinc-300 relative group">
+                    <li className="lg:text-xl cursor-pointer text-zinc-300">
+                      <p className="cursor-pointer text-zinc-300 relative group">
                         Locations
                         <span className="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-zinc-200 duration-300 group-hover:w-3/6"></span>
                         <span className="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-zinc-200 duration-300 group-hover:w-3/6"></span>
                       </p>
                     </li>
-                    <li className="text-xl cursor-pointer text-zinc-300">
-                      <p className="text-xl cursor-pointer text-zinc-300 relative group">
+                    <li className="lg:text-xl cursor-pointer text-zinc-300">
+                      <p className="cursor-pointer text-zinc-300 relative group">
                         About
                         <span className="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-zinc-200 duration-300 group-hover:w-3/6"></span>
                         <span className="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-zinc-200 duration-300 group-hover:w-3/6"></span>
                       </p>
                     </li>
-                    <li className="text-xl cursor-pointer text-zinc-300">
-                      <p className="text-xl cursor-pointer text-zinc-300 relative group">
+                    <li className="lg:text-xl cursor-pointer text-zinc-300">
+                      <p className="cursor-pointer text-zinc-300 relative group">
                         Contact
                         <span className="absolute -bottom-1 left-1/2 w-0 transition-all h-0.5 bg-zinc-200 duration-300 group-hover:w-3/6"></span>
                         <span className="absolute -bottom-1 right-1/2 w-0 transition-all h-0.5 bg-zinc-200 duration-300 group-hover:w-3/6"></span>
@@ -94,19 +95,29 @@ export default function Footer({}: Props) {
             </div>
             {/* bottom middle */}
             <div className="w-full h-1/4 border-t-2 border-zinc-200">
-              <div className="h-full w-full flex justify-between">
-                <div className="w-2/6 h-full flex p-3">
-                  <div className="w-1/2 h-full border-r-[1px] border-zinc-400 flex items-center justify-end pr-5">
-                    <p className="text-2xl text-zinc-300">Instagram</p>
+              <div className="h-full w-full flex flex-col lg:flex-row justify-between">
+                <div className="w-full lg:w-2/6 h-full flex justify-center lg:justify-normal p-3">
+                  <Link
+                    href=""
+                    className="cursor-pointer w-full h-full border-r-[1px] border-zinc-400 flex items-center justify-end pr-5"
+                  >
+                    <p className="cursor-pointer lg:text-2xl text-zinc-300 pr-3">
+                      Instagram
+                    </p>
                     <Instagram />
-                  </div>
-                  <div className="w-1/2 h-full border-l-[1px] border-zinc-400 pl-5 flex items-center">
+                  </Link>
+                  <Link
+                    href=""
+                    className="cursor-pointer w-full h-full border-l-[1px] border-zinc-400 pl-5 flex items-center"
+                  >
                     <Facebook />
-                    <p className="text-2xl text-zinc-300">Facebook</p>
-                  </div>
+                    <p className="cursor-pointer lg:text-2xl text-zinc-300 pl-3">
+                      Facebook
+                    </p>
+                  </Link>
                 </div>
                 <div className="w-fit h-full flex items-center">
-                  <p className="text-xl text-zinc-500 text-pretty ">
+                  <p className="text-xl text-zinc-500 text-center text-pretty">
                     © 2024 Helios Derma Clinic. All Rights Reserved
                   </p>
                 </div>
--- app/Components/ServiceSection/ServiceSection.tsx
+++ app/Components/ServiceSection/ServiceSection.tsx
@@ -4,6 +4,7 @@ import ArrowIcon from "../Icons/ArrowIcon";
 import Link from "next/link";
 import ServiceImage from "./ServiceImage";
 import { AnimatePresence, motion } from "framer-motion";
+import useDimension from "@/app/hooks/useDimension";
 
 type Props = {};
 type serviceType = {
@@ -49,23 +50,26 @@ const services = [
 function ServiceSection({}: Props) {
   const [hovered, setHovered] = useState(false);
   const [hoveredImage, setHoveredImage] = useState(1);
+  const { isSmallScreen } = useDimension();
 
   function mouseEnter(service: serviceType) {
+    if (isSmallScreen) return;
     setHovered(true);
     setHoveredImage(service.index);
   }
   function mouseLeave() {
+    if (isSmallScreen) return;
     setHovered(false);
   }
 
   return (
-    <div className="min-h-[150vh] w-full relative">
+    <div className="min-h-[125svh] lg:min-h-[150vh] w-full relative">
       <AnimatePresence>
         {hovered ? <ServiceImage imageNumber={hoveredImage} /> : null}
       </AnimatePresence>
 
       {/* Services Table */}
-      <div className="min-h-[150vh] w-full flex flex-col items-center">
+      <div className="min-h-[125svh] lg:min-h-[150vh] w-full flex flex-col items-center">
         {services.map((service) => {
           return (
             <Link
@@ -80,18 +84,20 @@ function ServiceSection({}: Props) {
               <div className="z-10 absolute inset-0  bg-zinc-300/10 scale-y-0 transform duration-300 ease-in-out group-hover/service:scale-y-100 group-hover/service:cursor-pointer"></div>
               <div className="w-full flex scale-x-100 transform duration-300 ease-in-out group-hover/service:scale-x-95">
                 <div className="w-[10%] flex items-center justify-center">
-                  <p className="text-2xl text-zinc-400">{service.index + 1}</p>
+                  <p className="text-lg lg:text-2xl text-zinc-400">
+                    {service.index + 1}
+                  </p>
                 </div>
-                <div className=" h-full w-full flex items-center">
-                  <p className="text-5xl text-zinc-800 font-semibold">
+                <div className="h-full w-full flex items-center">
+                  <p className="text-3xl pl-5 lg:text-5xl text-zinc-800 font-semibold">
                     {service.service_name}
                   </p>
                 </div>
               </div>
-              <div className="w-fit text-nowrap flex -translate-x-10 opacity-0 items-center justify-center transform duration-300 ease-in-out group-hover/service:opacity-100 group-hover/service:translate-x-0">
+              <div className="hidden w-fit text-nowrap -translate-x-10 opacity-0 lg:flex items-center justify-center transform duration-300 ease-in-out group-hover/service:opacity-100 group-hover/service:translate-x-0">
                 <p className="text-xl">More Info</p>
               </div>
-              <div className="w-[10%] flex items-center justify-center">
+              <div className="hidden w-[10%] md:flex items-center justify-center">
                 <ArrowIcon className="size-7 font-bold text-zinc-800 transform duration-300 ease-in-out group-hover/service:-rotate-45" />
               </div>
             </Link>
