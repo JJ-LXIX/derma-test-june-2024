3054060c6aad5080aea4e81bfb2d81d51fb3712c
Jagrath Shetty <jagrathshettylxix@gmail.com>
2024-07-14 20:02:38 +0530

    ui : adjusted to mobile screens

--- app/Components/Icons/Facebook.tsx
+++ app/Components/Icons/Facebook.tsx
@@ -12,7 +12,7 @@ export default function Facebook({}: Props) {
       strokeWidth="1"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className="w-4/6 h-4/6"
+      className="hidden lg:block lg:w-1/6 lg:h-1/6 cursor-pointer"
     >
       <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
     </svg>
--- app/Components/Icons/Instagram.tsx
+++ app/Components/Icons/Instagram.tsx
@@ -12,7 +12,7 @@ export default function Instagram({}: Props) {
       strokeWidth="1"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className="w-4/6 h-4/6"
+      className="hidden lg:block lg:w-1/6 lg:h-1/6 cursor-pointer"
     >
       <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
       <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
